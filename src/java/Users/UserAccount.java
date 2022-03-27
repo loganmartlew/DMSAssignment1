71f86bb39aa20153ccfd89be80fb3c0fc83384a3
Coen <48755627+Nyxxi@users.noreply.github.com>
2022-03-27 15:46:38 +1300

    Login stuff

--- src/java/Users/UserAccount.java
+++ src/java/Users/UserAccount.java
@@ -1,9 +1,5 @@
 package Users;
 
-import conn.ConnInit;
-import java.sql.*;
-import java.util.logging.Logger;
-
 /**
  *
  * @author coen
@@ -17,82 +13,15 @@ public class UserAccount {
 
     public UserAccount() {
     }
-
-    public void createUser(String uName, String pWord) {
-
-        Connection conn;
-        ConnInit init = new ConnInit();
-
-        try {
-            conn = init.startDB();
-
-            String user = uName;
-            String pass = pWord;
-
-            PreparedStatement st = conn.prepareStatement("SELECT * from vyd3379_userInfo where username=?");
-            st.setString(1, user);
-            ResultSet results = st.executeQuery();
-            
-            if (results.next()) {
-                System.out.println("user already exist in database!");
-
-            } else {
-                String query = "INSERT into vyd3379_userInfo where userName=? and password=?";
-
-                PreparedStatement ps = conn.prepareStatement(query);
-                ps.setString(1, user);
-                ps.setString(2, pass);
-
-                ps.execute();
-            }
-
-        } catch (SQLException ex) {
-            Logger.getLogger(ex.getMessage());
-
-        }
-    }
-
-    public void logoutSystem() {
-        isOnline = false;
-    }
-
-    public Boolean loginSystem() {
-
-        Connection conn;
-        ConnInit init = new ConnInit();
-
-        try {
-            conn = init.startDB();
-
-            PreparedStatement pS = conn.prepareStatement("SELECT userName from vyd3379_userInfo where userName=? and password=?");
-            pS.setString(1, userName);
-            pS.setString(2, password);
-
-            ResultSet results = pS.executeQuery();
-
-            if (results.next()) {
-                userName = results.getString(1);
-                password = results.getString(2);
-                isOnline = true;
-
-            } else {
-                pS.close();
-                conn.close();
-                isOnline = false;
-
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ex.getMessage());
-
-        }
-        return isOnline;
-    }
-
     /*
     *Below is getters and setters for UserAccount class
      */
     public boolean getIsOnline() {
         return isOnline;
+    }
+    
+    public void setIsOnline(Boolean on){
+        this.isOnline = on;
     }
 
     public String getUserName() {
---
+++ web/invalidLogin.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : invalidLogin
+    Created on : 27/03/2022, 3:22:46 pm
+    Author     : coen-
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Invalid Login</title>
+    </head>
+    <body>
+        Sorry you are not a registered user! Please sign up first
+    </body>
+</html>
